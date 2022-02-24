3aa03939b12f236f9ae500c22700809933046bef
zzung00 <lee759444@gmail.com>
2022-02-24 16:22:54 +0900

    Initial Commit

---
+++ HeyTaxi-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeyTaxi-iOS
+//
+//  Created by 이정현 on 2022/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeyTaxi-iOS/HeyTaxi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyTaxi_iOSApp.swift
+//  HeyTaxi-iOS
+//
+//  Created by 이정현 on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HeyTaxi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
