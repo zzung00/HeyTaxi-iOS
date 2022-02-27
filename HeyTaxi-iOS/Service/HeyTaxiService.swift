f23a7593fd1a1e5028c8e49df54529bd976321d9
zzung00 <lee759444@gmail.com>
2022-02-27 14:22:53 +0900

    update verify service

--- HeyTaxi-iOS/Service/HeyTaxiService.swift
+++ HeyTaxi-iOS/Service/HeyTaxiService.swift
@@ -37,6 +37,7 @@ class HeyTaxiService {
         "Accept" : "application/json"
     ]
     
+    //서버 연결 확인
     func serverConnect(completion: @escaping (Bool) -> Void) {
         AF.request(HeyTaxiService.baseUrl, method: .get, encoding: JSONEncoding.default).responseJSON {
             response  in
@@ -46,10 +47,35 @@ class HeyTaxiService {
         }
     }
     
+    //전화번호를 통한 인증 요청 단계
     func verifyRequest(phone: String, completion: @escaping (VerifyResponse) -> Void) {
         let url: String = HeyTaxiService.baseUrl + "/api/verify/request"
         let body: Parameters = [
             "phone": phone
+        ]
+        AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default).responseJSON {
+            response in
+            switch response.result {
+            case.success(let value):
+                do {
+                    let data = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                    let decoder = JSONDecoder()
+                    let verifyResponse = try decoder.decode(VerifyResponse.self, from: data)
+                    completion(verifyResponse)
+                } catch {}
+            default:
+                return
+            }
+        }
+    }
+    
+    //문자로 수신된 인증번호를 통한 인증 확인 단계
+    func verify(phone: String, clientSecret: String, code: String, completion: @escaping (VerifyResponse) -> Void) {
+        let url: String = HeyTaxiService.baseUrl + "/api/verify/verify"
+        let body: Parameters = [
+            "phone": phone,
+            "clientSecret": clientSecret,
+            "code": code
         ]
         AF.request(url, method: .post, parameters: body, encoding: JSONEncoding.default).responseJSON {
             response in
