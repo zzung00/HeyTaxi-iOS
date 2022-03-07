1d8965e0f90fcd6b0aa1bdd59f8a0dc56fbfa415
zzung00 <lee759444@gmail.com>
2022-03-07 14:24:41 +0900

    create mainView

---
+++ HeyTaxi-iOS/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  HeyTaxi-iOS
+//
+//  Created by 이정현 on 2022/03/07.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
