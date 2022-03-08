35376b927f1ec64d30b7409c4f9d63ff4a953138
zzung00 <lee759444@gmail.com>
2022-03-08 18:10:17 +0900

    add: show user location on map

--- HeyTaxi-iOS/View/MainView.swift
+++ HeyTaxi-iOS/View/MainView.swift
@@ -6,10 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MainView: View {
+    @StateObject private var viewModel = MainViewModel()
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: MapDefaults.latitude, longitude: MapDefaults.longitude),
+        span: MKCoordinateSpan(latitudeDelta: MapDefaults.zoom, longitudeDelta: MapDefaults.zoom))
+    
+    private enum MapDefaults {
+        static let latitude = 35.96322239939191
+        static let longitude = 126.98783602642584
+        static let zoom = 0.5
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: ProfileView()) {
+                        Image(systemName: "person")
+                    }
+                }
+            }
+            .onAppear {
+                viewModel.loadMe()
+                viewModel.requestPermission()
+                viewModel.registerSocket()
+            }
+        }.navigationBarHidden(true)
     }
 }
 
