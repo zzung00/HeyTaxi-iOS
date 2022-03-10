04ea0e05024fb50bc2f4df6a3b59931e58d1be7f
zzung00 <lee759444@gmail.com>
2022-03-10 15:39:05 +0900

    complete: update profile service

--- HeyTaxi-iOS/View/ProfileView.swift
+++ HeyTaxi-iOS/View/ProfileView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var name = ""
     @State private var username = ""
     @StateObject private var viewModel = ProfileViewModel()
     @Environment(\.presentationMode) var presentationMode
@@ -15,24 +16,56 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .center, spacing: 10) {
+                Image(systemName: "person")
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(.mainGreen)
+                    .frame(width: 200, height: 200)
                 
+                Spacer()
+                
+                TextField("성명", text: $name)
+                    .frame(width: 300, height: 50)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.default)
+                    .disableAutocorrection(true)
+                
+                TextField("사용자 이름", text: $username)
+                    .frame(width: 300, height: 50)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.default)
+                    .disableAutocorrection(true)
+                
+                Spacer()
             }
             .onAppear {
                 viewModel.loadMe {
                     user in
-                    
+                    name = user.name!
+                    username = user.username!
                 }
             }
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
+                ToolbarItem(placement: .navigationBarLeading) {
                     Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
                         Text("취소")
                             .bold()
                             .foregroundColor(.red)
                     }
                 }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        viewModel.user?.name = name
+                        viewModel.updateMe(user: viewModel.user!)
+                        
+                    }) {
+                        Text("저장")
+                            .bold()
+                    }
+                }
             }
-        }
+        }.navigationBarHidden(true)
     }
 }
 
